4c398696bc54be1fb2b3a769b40ae9bcd8d7426d
Romina Beca <rominabeca.v@gmail.com>
2024-07-01 22:39:31 -0400

    version 1

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import joyasRoutes from "./routes/joyasRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api", joyasRoutes);
+
+app.listen(PORT, () => console.log(`Servidor encendido en el puerto ${PORT}!`));
